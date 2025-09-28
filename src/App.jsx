04e6cd54f1b9fac30b22c755d37900f464f010dd
fuadraihan10 <contact.fuadraihan@gmail.com>
2025-09-29 01:15:00 +0600

    added testimonial

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header/Header";
 import Hero from "./Hero/hero";
 import Service1 from "./Service1/Service1";
 import Service2 from "./Service2/service2";
+import Testimonial from "./Testimonial/Testimonial";
 
 function App() {
   return ( 
@@ -12,6 +13,7 @@ function App() {
     <CompaniesLogos />
     <Service1 />
     <Service2 />
+    <Testimonial />
   </div> );
 }
 
