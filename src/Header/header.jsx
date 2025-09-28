018b37b0cce749b719dfe8c66e92863b46267151
fuadraihan10 <contact.fuadraihan@gmail.com>
2025-09-28 22:58:40 +0600

    fixing a deployment error

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -11,7 +11,7 @@ function Header() {
 
                 {/* logo */}
                 <a href="#" className="flex items-center">
-                        <img src="./assets/logo.svg" className="h-6 mr-3 sm:h-9"
+                        <img src="/assets/logo.svg" className="h-6 mr-3 sm:h-9"
                             alt="Landwind Logo" />
                         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                             Learn with Sumit</span>
